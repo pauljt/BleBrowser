fff9b485f659a25fb7a601b6b97b51c987cdb9de
daphtdazz <daphtdazz@icloud.com>
2019-09-07 11:59:41 +0100

    add template code for single view web ble app

---
+++ Single View Web BLE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Single View Web BLE
+//
+//  Created by David Park on 07/09/2019.
+//  Copyright © 2019 David Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
