c130a9801469a44b20218fa6d8eee3cb29ac0275
daphtdazz <daphtdazz@icloud.com>
2022-03-27 16:49:15 +0100

    Improve instructions in the https.py file for creating TLS certs locally

--- https.py
+++ https.py
@@ -1,21 +1,40 @@
 """
 Run an HTTPS server serving the contents of the directory.
 
-You'll need to generate a key and certificate file, which for development purposes can be
-self-signed, something like:
+You'll need to generate a key and certificate file. On a Mac you should do this by following
+the instructions in:
 
-    openssl \
-        req \
-        -x509 \
-        -newkey rsa:4096 \
-        -keyout key.pem \
-        -out cert.pem \
-        -sha256 \
-        -nodes \
-        -subj '/CN=localhost' \  # or whatever local domain name you wish to use
-        -days 365
+https://developer.apple.com/library/archive/technotes/tn2326/_index.html
 
-Then to install it simply email yourself the cert.pem file.
+First "Creating a Certificate Authority" and then "Issuing a [server] Digital Identity"
+
+This will generate a self-signed root Certificate Authority (CA) certificate and a signed server
+certificate (digital identity) to be used by your server.
+
+You then need to export the certificate authority certificate
+as a .cer file through KeyChain by
+
+- right click the CA certificate
+- hit Export...
+- select the .cer format
+
+Then add it to you test iOS device by
+
+- emailing it to yourself
+- tapping on the .cer attachment
+- installing it and trusting it
+- enabling it through Settings -> General -> About -> Certificate Trust Settings
+
+Then you need to export the signed server cert, this time using format
+"Personal Information Exchange" (.p12) so that it includes the private key, then convert it to
+pem using openssl like this:
+
+    openssl pkcs12 -in your-export.p12 -nocerts -nodes -out your-server.key
+    openssl pkcs12 -in your-export.p12 -nokeys -nodes -out your-server.cer
+
+You can then run this server like this:
+
+    python3 https.py --host 192.168.1.102 --port 4443 your-server.key your-server.cer
 """
 import argparse
 import http.server
