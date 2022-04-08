ca57518bf751e5310e9e02b217125cda566a7ca3
daphtdazz <daphtdazz@icloud.com>
2022-04-08 18:46:14 +0100

    Keep the URL text field full width with manual constraints

--- BasicBrowser/URLTextField.swift
+++ BasicBrowser/URLTextField.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class URLTextField: UITextField {
     var alreadyWasFirstResponder = false
+    var superViewConstraints: [NSLayoutConstraint] = []
 
     override func becomeFirstResponder() -> Bool {
         let answer = super.becomeFirstResponder()
@@ -22,5 +23,26 @@ class URLTextField: UITextField {
         let answer = super.resignFirstResponder()
         self.alreadyWasFirstResponder = false
         return answer
+    }
+
+    override func didMoveToSuperview() {
+        if self.superViewConstraints.count > 0 {
+            NSLayoutConstraint.deactivate(self.superViewConstraints)
+            // don't necessarily delete them:
+            // we may be able to reuse them
+        }
+
+        if let sv = self.superview {
+            if self.superViewConstraints.count == 0
+                || self.superViewConstraints[0].secondItem as? UIView != sv {
+                // need new constraints
+                self.superViewConstraints = [
+                    self.leadingAnchor.constraint(equalTo: sv.leadingAnchor, constant: 8),
+
+                    self.trailingAnchor.constraint(equalTo: sv.trailingAnchor, constant: -8),
+                ]
+            }
+            NSLayoutConstraint.activate(self.superViewConstraints)
+        }
     }
 }
